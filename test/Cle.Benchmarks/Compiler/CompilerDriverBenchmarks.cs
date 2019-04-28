598e2881120c37cb4c24e5784881f9def8374483
Petri Laarne <polsys@users.noreply.github.com>
2019-04-28 19:19:35 +0300

    Add an end-to-end compiler benchmark

--- test/Cle.Benchmarks/Compiler/CompilerDriverBenchmarks.cs
+++ test/Cle.Benchmarks/Compiler/CompilerDriverBenchmarks.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using Cle.Compiler;
 
 namespace Cle.Benchmarks.Compiler
 {
     public class CompilerDriverBenchmarks
     {
-        private readonly SingleFileProvider _almostSemanticallyValidSourceProvider;
+        private readonly SingleFileProvider _invalidSourceProvider;
         private readonly NullOutputProvider _nullOutputProvider;
 
         public CompilerDriverBenchmarks()
         {
-            _almostSemanticallyValidSourceProvider = new SingleFileProvider(AlmostSemanticallyValidSource);
+            _invalidSourceProvider = new SingleFileProvider(SemanticallyInvalidSource);
             _nullOutputProvider = new NullOutputProvider();
         }
 
@@ -22,86 +18,20 @@ namespace Cle.Benchmarks.Compiler
         public int CompileSingleFileWithSemanticError()
         {
             var result = CompilerDriver.Compile(new CompilationOptions("."),
-                _almostSemanticallyValidSourceProvider, _nullOutputProvider);
+                _invalidSourceProvider, _nullOutputProvider);
             return result.Diagnostics.Count;
         }
         
-        // TODO: This should be extended with more syntactic elements once they exist
         // Two errors:
-        //   - no function annotated with [EntryPoint] (TODO once implemented)
-        //   - the last function has a type mismatch
-        private const string AlmostSemanticallyValidSource = @"
-namespace Benchmarks::Compiler::AlmostValidFile;
-
-public int32 GetTheAnswerToEverything()
-{
-    bool isTrue = true;
-    if (isTrue) { return 42; }
-    else { return 41; }
-}
-
-public bool SwapBoolsAround()
-{
-    bool one = true;
-    bool two = true;
-    if (one) { two = false; }
-    bool three = two;
-    if (three) { return one; } else { return two; }
-}
+        //   - no function annotated with [EntryPoint]
+        //   - the function has a type mismatch
+        private const string SemanticallyInvalidSource = @"
+namespace InvalidFile;
 
 private bool TypeMismatch()
 {
     return 0;
 }
 ";
-
-        private class SingleFileProvider : ISourceFileProvider
-        {
-            private static readonly string[] s_filenames = { "file.cle" };
-            private readonly Memory<byte> _source;
-
-            public SingleFileProvider(string source)
-            {
-                _source = Encoding.UTF8.GetBytes(source).AsMemory();
-            }
-
-            public bool TryGetFilenamesForModule(string moduleName, out IEnumerable<string> filenames)
-            {
-                filenames = s_filenames;
-                return true;
-            }
-
-            public bool TryGetSourceFile(string filename, out Memory<byte> fileBytes)
-            {
-                if (filename == "file.cle")
-                {
-                    fileBytes = _source;
-                    return true;
-                }
-                else
-                {
-                    fileBytes = Memory<byte>.Empty;
-                    return false;
-                }
-            }
-        }
-
-        private class NullOutputProvider : IOutputFileProvider
-        {
-            public Stream GetExecutableStream()
-            {
-                return null;
-            }
-
-            public TextWriter GetDisassemblyWriter()
-            {
-                return null;
-            }
-
-            public TextWriter GetDebugFileWriter()
-            {
-                return null;
-            }
-        }
     }
 }
---
+++ test/Cle.Benchmarks/Compiler/EndToEndBenchmarks.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using Cle.Compiler;
+
+namespace Cle.Benchmarks.Compiler
+{
+    public class EndToEndBenchmarks
+    {
+        private readonly SingleFileProvider _sourceProvider;
+        private readonly NullOutputProvider _nullOutputProvider;
+        
+        public EndToEndBenchmarks()
+        {
+            _sourceProvider = new SingleFileProvider(Source);
+            _nullOutputProvider = new NullOutputProvider();
+        }
+
+        [Benchmark]
+        public int CompileSingleFileProgram()
+        {
+            var result = CompilerDriver.Compile(new CompilationOptions("."),
+                _sourceProvider, _nullOutputProvider);
+
+            return result.Diagnostics.Count;
+        }
+
+        private const string Source = @"
+// This the Functional/CollatzOnCollatz integration test
+
+namespace CollatzOnCollatz;
+
+[EntryPoint]
+public int32 Main()
+{
+    return Collatz(Collatz(9));
+}
+
+private int32 Collatz(int32 num)
+{
+    int32 i = 0;
+    while (num != 1)
+    {
+        i = i + 1;
+        if (num % 2 == 0)
+        {
+            num = num / 2;
+        }
+        else
+        {
+            num = 3*num + 1;
+        }
+    }
+    return i;
+}
+";
+    }
+}
