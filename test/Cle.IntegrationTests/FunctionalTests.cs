55afa354c3c140768a329d7c68e40296a05587ef
Petri Laarne <polsys@users.noreply.github.com>
2019-05-01 17:36:10 +0300

    Add some more functional test cases

--- test/Cle.IntegrationTests/FunctionalTests.cs
+++ test/Cle.IntegrationTests/FunctionalTests.cs
@@ -8,9 +8,12 @@ namespace Cle.IntegrationTests
         private const string BaseDirectory = "TestCases/Functional";
         private const int ExecutionTimeOutMilliseconds = 1000;
 
+        [TestCase("Binomial", 210)]
+        [TestCase("DeepLoop", 210)]
         [TestCase("CollatzOnCollatz", 20)]
         [TestCase("IterativeFibonacci", 55)]
         [TestCase("RecursiveFibonacci", 55)]
+        [TestCase("SumOfMultiples", 233168)]
         public void Functional(string testCase, int expectedReturnCode)
         {
             new TestRunner(Path.Combine(BaseDirectory, testCase))
